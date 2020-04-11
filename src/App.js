1ab805fa9ec546dc7cb6847866fcaf0a01a6d844
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2020-04-11 17:02:04 +0200

    Implementing a Form Component as an uncontrolled component using refs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+
 import Header from './components/layout/Header';
 import { Provider } from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
