90d77f0dc10276f60f0a4233f842fab7238def3a
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2020-04-11 14:51:47 +0200

    Adding A form to the app as a controlled component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Contacts from './components/contacts/Contacts';
+import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
 import { Provider } from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,6 +17,7 @@ function App() {
       <div className='App'>
         <Header branding='Contact Manager' />
         <div className='container'>
+          <AddContact />
           <Contacts />
         </div>
       </div>
