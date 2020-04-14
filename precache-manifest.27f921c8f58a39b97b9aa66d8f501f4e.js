8e7553908b86a8306dc8a6409842cf6c42d1f202
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2020-04-14 18:14:35 +0200

    Updates

---
+++ precache-manifest.27f921c8f58a39b97b9aa66d8f501f4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be29b4d2bd843d750811f54d8969b4b7",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "903f008ca4a5105c9ba5",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "44cf7d1fe97cc74b28c5",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "903f008ca4a5105c9ba5",
+    "url": "/contactmanager/static/js/2.2aee5873.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.2aee5873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cf7d1fe97cc74b28c5",
+    "url": "/contactmanager/static/js/main.33829425.chunk.js"
+  },
+  {
+    "revision": "71d3a297f4256315968e",
+    "url": "/contactmanager/static/js/runtime-main.d7d94ad5.js"
+  }
+]);
